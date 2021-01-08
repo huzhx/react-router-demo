4ae76bb26044b32a3c1ce7727827e7864e86cd0a
Zhaoxian Hu <huzhx@uci.edu>
2021-01-08 15:10:04 -0800

    add BasicRoutingHandler component

---
+++ src/BasicRoutingHandler.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+
+export default function BasicRoutingHandler() {
+  return (
+    <BrowserRouter>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
